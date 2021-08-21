51a54a6c30f9e67bc114fb318b9f32efd3e1e59a
Rakibul santo <SANTODON176@gmail.com>
2021-08-21 12:55:21 +0600

    remove css file

--- js/scripts.js
+++ js/scripts.js
@@ -1,112 +1,81 @@
 
 
-function updatePrice(getId, price){
-  const currentPrice = document.getElementById(getId);
-  currentPrice.innerText = price;
 
-  // total price update here
-  const allPrice = document.getElementById('all-price');
-  const totalPriceInNumber = parseFloat(allPrice.innerText);
+// updateTotal price
+function updateTotalPrice() {
+  const memoryPrice = document.getElementById('memory-cost');
+  const storagePrice = document.getElementById('storage-cost');
+  const deliveryCost = document.getElementById('delivery-cost');
+  const totalPrice = document.getElementById('all-Total-price');
 
- 
-   if(price == 180 ){
-      allPrice.innerText = totalPriceInNumber + price;
-  }
+  const allTotalPrice = 1299 + parseFloat(memoryPrice.innerText) + parseFloat(storagePrice.innerText) + parseFloat(deliveryCost.innerText);
 
-  // else if(price == 100 ){
-  //   allPrice.innerText = totalPriceInNumber +100;
-  // }
-  // else if(price == 0  && totalPriceInNumber > 1299){
-  //   allPrice.innerText = totalPriceInNumber -180;
-  // }
+  totalPrice.innerText = allTotalPrice;
+  return allTotalPrice;
+
 }
 
-
-//frist memort btn
+//Memory cost price 
 document.getElementById('frist-memory-btn').addEventListener('click', function(){
-  updatePrice('memory-cost', 0);
-});
+const memoryCost1 = document.getElementById('memory-cost');
+memoryCost1.innerText = '0';
+updateTotalPrice() 
 
-// second memory btn
+})
+
 document.getElementById('second-memory-btn').addEventListener('click', function(){
-  updatePrice('memory-cost', 180);
-});
+const memoryCost2 = document.getElementById('memory-cost');
+memoryCost2.innerText = '180';
+updateTotalPrice() 
+})
 
+// storage Cost price
 
-// function updateStoragePrice(getIdCost, storagePrice){
-//   const currentPrice = document.getElementById(getIdCost);
-//   currentPrice.innerText = storagePrice;
-
-//   // total price update here
-//   const allCost = document.getElementById('all-price');
-//   const totalCostNumber = parseFloat(allCost.innerText);
-
- 
-//    if(storagePrice == 180  && totalCostNumber == 1299){
-//       allCost.innerText = totalCostNumber + storagePrice;
-//   }
-
-  
-//   else if(storagePrice == 0 ){
-//     allCost.innerText = totalCostNumber + storagePrice;
-//   }
-//   else{
-//     allCost.innerText = totalCostNumber + storagePrice;
-//   }
-
-
-// }
-
-//frist storage btn
 document.getElementById('frist-ssd-btn').addEventListener('click', function(){
-  updatePrice('storage-cost', 0);
-});
-
-// second storage  btn
+  const storageCost1 = document.getElementById('storage-cost');
+  storageCost1.innerText = '0';
+  updateTotalPrice() 
+})
 document.getElementById('second-ssd-btn').addEventListener('click', function(){
-  updatePrice('storage-cost', 100);
-});
-// secondstorage  btn
+  const storageCost2 = document.getElementById('storage-cost');
+  storageCost2.innerText = '100';
+  updateTotalPrice() 
+})
 document.getElementById('third-ssd-btn').addEventListener('click', function(){
-  updatePrice('storage-cost', 180);
-});
+  const storageCost3 = document.getElementById('storage-cost');
+  storageCost3.innerText = '180';
+  updateTotalPrice() 
+})
 
 
-function updateDeliveryCost(getId, deliveruCost){
-  const currentPrice = document.getElementById(getId);
-  currentPrice.innerText = deliveruCost;
 
-  // total price update here
-  const allCost = document.getElementById('all-price');
-  const totalCostPrice = parseFloat(allCost.innerText);
+//delivery Cost price 
 
- 
-  //  if(deliveruCost == 0 ){
-  //   allCost.innerText = totalCostPrice -20;
-  // }
-
-  // else if(deliveruCost == 20){
-  //   allCost.innerText = totalCostPrice +20;
-  // }
+document.getElementById('frist-delivery-btn').addEventListener('click', function(){
+  
+const deliveryCost1 = document.getElementById('delivery-cost');
+deliveryCost1.innerText = '0';
+updateTotalPrice() 
+})
+document.getElementById('second-delivery-btn').addEventListener('click', function(){
+  const deliveryCost2 = document.getElementById('delivery-cost');
+  deliveryCost2.innerText = '20';
+  updateTotalPrice() 
+})
 
 
-  if(price == 20 ){
-    allPrice.innerText = totalPriceInNumber + price;
-}
+// discount total price 
 
+document.getElementById('promo-code-btn').addEventListener('click', function () {
+  const finalTotalAmount = document.getElementById('final-total-price');
 
-else if(price == 0  && totalPriceInNumber > 1299){
-  allPrice.innerText = totalPriceInNumber -20;
-}
-}
+  const promoCode = document.getElementById('copun-input');
+  const totalDiscount = (updateTotalPrice() / 100) * 20;
+  const finalDiscount = updateTotalPrice() - totalDiscount;
+  if (promoCode.value == 'stevekaku') {
+      finalTotalAmount.innerText = finalDiscount;
+  
+  }
+  promoCode.value = '';
 
-
-//frist memort btn
-document.getElementById('prime-delivery-btn').addEventListener('click', function(){
-  updatePrice('delivery-cost', 0);
-});
-
-// second memory btn
-document.getElementById('delivery-btn').addEventListener('click', function(){
-  updatePrice('delivery-cost', 20);
-});
+})
